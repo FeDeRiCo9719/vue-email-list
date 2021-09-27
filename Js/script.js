3d3c4cac310a1b72942290faff1b96f74a2171a4
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-09-27 21:45:30 +0200

    get url

--- Js/script.js
+++ Js/script.js
@@ -1,4 +1,20 @@
+/*
+Attraverso l'apposita API di Boolean 
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
 var app = new Vue({
-    el: '',
-    data: {}
+    el: '#root',
+    data: {},
+    methods: {},
+
+    mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(function (response) {
+            console.log(response.status);
+        });
+    }
 });
